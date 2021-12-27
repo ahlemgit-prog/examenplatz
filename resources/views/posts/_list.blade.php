062f77787eaef5d79e22be9df85aa84c2952196d
halouma <essidahlem.info1997@gmail.com>
2021-12-27 18:57:29 +0100

    platz

--- resources/views/posts/_list.blade.php
+++ resources/views/posts/_list.blade.php
@@ -6,7 +6,7 @@
               'post' => $post->id,
               'slug' => \Illuminate\Support\Str::slug($post->title)
             ]) }}">
-          <img src="{{ asset('storage/app/public/posts' . $post->image) }}" alt="" />
+          <img src="{{ asset('storage/app/public/posts' . $post->content) }}" alt="" />
           <dl>
             <dt>{{ $post->title }}</dt>
             <dd>{{ $post->excerpt }}</dd>
