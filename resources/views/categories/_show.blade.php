fcd0ab881b5f67d38bc228cd16e96aa922fa67dd
halouma <essidahlem.info1997@gmail.com>
2021-12-28 22:43:30 +0100

    platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -45,7 +45,7 @@
                   'slug' => \Illuminate\Support\Str::slug($post->title)
                 ]) }}">
                 <div class="image-morefrom-3">
-                  <img src="{{asset('storage/posts' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
+                  <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
                 </div>
               </a>
             <a href="{{ route('posts.show', [
@@ -53,7 +53,7 @@
                   'slug' => \Illuminate\Support\Str::slug($post->title)
                 ]) }}">
                 <div class="image-morefrom-4">
-                  <img src="{{asset('storage/posts' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
+                  <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
                 </div>
               </a>
         </div>
