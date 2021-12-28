d049720bd9706d977bf938fc185b54834bca1e15
halouma <essidahlem.info1997@gmail.com>
2021-12-28 15:50:18 +0100

    platz

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -24,7 +24,7 @@
 
       <div class="work">
         <figure class="white">
-             <img src="{{asset('storage/' . $post->image)}}" alt=""/>
+             <img src="{{asset('storage/' . $post->body)}}" alt=""/>
         </figure>
 
         {{-- Aside du post --}}
