5027ef306dfc68dd1bf66ce8f3d9713df85a4603
halouma <essidahlem.info1997@gmail.com>
2021-12-29 15:04:41 +0100

    platz

--- resources/views/template/partials/_nav.blade.php
+++ resources/views/template/partials/_nav.blade.php
@@ -2,8 +2,20 @@
 <div id="wrapper-navbar">
 		<div class="navbar object">
 			<div id="wrapper-sorting">
-				{{-- Pages du post ——}}
-				@include('pages._index')
+				<div id="wrapper-title-1">
+				  <div class="top-rated object">Top-rated</div>
+				    <div id="fleche-nav-1"></div>
+				</div>
+
+				<div id="wrapper-title-2">
+				  <a href="#"><div class="recent object">Recent</div></a>
+				      <div id="fleche-nav-2"></div>
+				</div>
+
+				<div id="wrapper-title-3">
+				  <a href="#"><div class="oldies object">Oldies</div></a>
+				      <div id="fleche-nav-3"></div>
+				</div>
 			</div>
 			<div id="wrapper-bouton-icon">
 				{{-- Categories du post --}}
