64b8300d0d91a853bb4fb8a6f1f2b0926301e697
halouma <essidahlem.info1997@gmail.com>
2021-12-29 14:39:42 +0100

    platz

---
+++ resources/views/pages/_index.blade.php
@@ -0,0 +1,14 @@
+<div id="wrapper-title-1">
+  <div class="top-rated object">Top-rated</div>
+    <div id="fleche-nav-1"></div>
+</div>
+
+<div id="wrapper-title-2">
+  <a href="#"><div class="recent object">Recent</div></a>
+      <div id="fleche-nav-2"></div>
+</div>
+
+<div id="wrapper-title-3">
+  <a href="#"><div class="oldies object">Oldies</div></a>
+      <div id="fleche-nav-3"></div>
+</div>
--- resources/views/template/partials/_nav.blade.php
+++ resources/views/template/partials/_nav.blade.php
@@ -2,22 +2,11 @@
 <div id="wrapper-navbar">
 		<div class="navbar object">
 			<div id="wrapper-sorting">
-				<div id="wrapper-title-1">
-					<div class="top-rated object">Top-rated</div>
-						<div id="fleche-nav-1"></div>
-				</div>
-
-				<div id="wrapper-title-2">
-					<a href="#"><div class="recent object">Recent</div></a>
-							<div id="fleche-nav-2"></div>
-				</div>
-
-				<div id="wrapper-title-3">
-					<a href="#"><div class="oldies object">Oldies</div></a>
-							<div id="fleche-nav-3"></div>
-				</div>
+				{{-- Pages du post ——}}
+				@include('pages._index', [
+				'categories' => \App\Models\Page::orderBy('id', 'ASC')->get()
+				])
 			</div>
-
 			<div id="wrapper-bouton-icon">
 				{{-- Categories du post --}}
 				@include('categories._index', [
