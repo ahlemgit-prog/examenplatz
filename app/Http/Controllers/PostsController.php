667d35b683ca5db483139be180c55a97d973c74c
halouma <essidahlem.info1997@gmail.com>
2021-12-29 18:00:01 +0100

    platz

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 class PostsController extends Controller
 {
   public function index() {
-    $posts = Post::orderBy('id', 'desc')
+    $posts = Post::orderBy('id', 'asc')
                  ->take(20)
                  ->get();
     return view('posts.index', compact('posts'));
