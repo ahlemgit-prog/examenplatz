834784a494f8a4f91110a2f7776142dee5827463
halouma <essidahlem.info1997@gmail.com>
2021-12-29 20:45:15 +0100

    platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -6,7 +6,7 @@
 
     <div class="wrapper-weight">
       <div class="icon-weight"><img src="{{asset('/assets/img/icon-weight.svg')}}" alt="" width="20" height="23"/></div>
-        <div class="text-weight">60 Mo</div>
+        <div class="text-weight">{{ $post->size }} Mo</div>
     </div>
 
     <div class="wrapper-desc">
