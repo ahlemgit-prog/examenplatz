1b4fb60c3958836eee36612e350200a8cf24e54a
halouma <essidahlem.info1997@gmail.com>
2022-03-09 17:44:53 +0100

    Platz

--- resources/views/categories/aside.blade.php
+++ resources/views/categories/aside.blade.php
@@ -1,62 +1,19 @@
-<div class="wrapper-text-description">
+@extends('template.index')
 
-    <div class="wrapper-file">
-        <div class="icon-file"><img src="{{asset('storage/posts/' .$post->meta_description)}}" alt="" width="21" height="21"/></div>
-          <div class="text-file">{{ $categorie->name }} [{{ count($categorie->posts) }}]</div>
-    </div>
+@section('title')
+    Free Grid Themes
+@endsection
 
-    <div class="wrapper-weight">
-      <div class="icon-weight"><img src="{{asset('/assets/img/icon-weight.svg')}}" alt="" width="20" height="23"/></div>
-        <div class="text-weight">{{ $post->size }} Mo</div>
-    </div>
+@section('content')
+<div id="wrapper-container">
 
-    <div class="wrapper-desc">
-      <div class="icon-desc"><img src="{{asset('/assets/img/icon-desc.svg')}}" alt="" width="24" height="24"/></div>
-        <div class="text-desc">{{ $post->excerpt }} </div>
-    </div>
+  <div class="container object">
 
-    <div class="wrapper-download">
-      <div class="icon-download"><img src="{{asset('/assets/img/icon-download.svg')}}" alt="" width="19" height="26"/></div>
-        <div class="text-download">
-          <a href="{{asset('storage/posts/' .$post->meta_keywords)}}" download><b>Download</b></a>
+    <div id="main-container-image">
+
+          @include('posts._list', ['posts' => $posts])
+
       </div>
     </div>
-
-    <div class="wrapper-morefrom">
-      <div class="text-morefrom">More from .{{ $categorie->slug }}</div>
-        <div class="image-morefrom">
-            <a href="{{ route('posts.show', [
-                'post' => $post->id,
-                'slug' => \Illuminate\Support\Str::slug($post->title)
-              ]) }}">
-              <div class="image-morefrom-1">
-                <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
-              </div>
-            </a>
-            <a href="{{ route('posts.show', [
-                  'post' => $post->id,
-                  'slug' => \Illuminate\Support\Str::slug($post->title)
-                ]) }}">
-                <div class="image-morefrom-2">
-                  <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
-                </div>
-              </a>
-            <a href="{{ route('posts.show', [
-                  'post' => $post->id,
-                  'slug' => \Illuminate\Support\Str::slug($post->title)
-                ]) }}">
-                <div class="image-morefrom-3">
-                  <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
-                </div>
-              </a>
-            <a href="{{ route('posts.show', [
-                  'post' => $post->id,
-                  'slug' => \Illuminate\Support\Str::slug($post->title)
-                ]) }}">
-                <div class="image-morefrom-4">
-                  <img src="{{asset('storage/posts/' . $post->meta_keywords)}}" alt="" width="430" height="330"/>
-                </div>
-              </a>
-        </div>
-    </div>
 </div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -28,7 +28,7 @@
         </figure>
 
         {{-- Aside du post --}}
-        @include('categories.aside', ['categorie' => $post->categorie])
+        @include('categories._show', ['categorie' => $post->categorie])
 
         {{-- Comments du post --}}
         @include('comments._index', ['comments' => $post->comments])
