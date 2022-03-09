869c7b7fa2414ed1637c2d033b69af4fb4d85be0
halouma <essidahlem.info1997@gmail.com>
2022-03-09 17:09:10 +0100

    Platz

--- resources/views/categories/aside.blade.php
+++ resources/views/categories/aside.blade.php
@@ -1,7 +1,7 @@
 <div class="wrapper-text-description">
 
     <div class="wrapper-file">
-        <div class="icon-file"><img src="{{asset('/assets/img/' .$categorie->icon)}}" alt="" width="21" height="21"/></div>
+        <div class="icon-file"><img src="{{asset('storage/posts/' .$categorie->icon)}}" alt="" width="21" height="21"/></div>
           <div class="text-file">{{ $categorie->name }} [{{ count($categorie->posts) }}]</div>
     </div>
 
