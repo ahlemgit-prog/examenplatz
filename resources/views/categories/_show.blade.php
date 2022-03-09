b4b7e46685b127f371eee8bade31f60481b538a0
halouma <essidahlem.info1997@gmail.com>
2022-03-09 18:11:32 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -1,1 +1,19 @@
+@extends('template.index')
 
+@section('title')
+    Free Grid Themes
+@endsection
+
+@section('content')
+<div id="wrapper-container">
+
+  <div class="container object">
+
+    <div id="main-container-image">
+
+          
+
+      </div>
+    </div>
+</div>
+@endsection
