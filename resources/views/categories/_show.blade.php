af35cc381a9a424b089b8921b1f22e5db8b7be84
halouma <essidahlem.info1997@gmail.com>
2022-03-09 19:00:41 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -1,1 +1,19 @@
+@extends('template.index')
 
+@section('title')
+    Free Grid
+@endsection
+
+@section('content')
+<div id="wrapper-container">
+
+  <div class="container object">
+
+    <div id="main-container-image">
+
+          <!-- @include('posts._list', ['posts' => $posts]) -->
+
+      </div>
+    </div>
+</div>
+@endsection
