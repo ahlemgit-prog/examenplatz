d3b067a9d6c3a38a4d06dde90156beab726a4af1
halouma <essidahlem.info1997@gmail.com>
2022-03-09 14:36:02 +0100

    Platz

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -28,7 +28,7 @@
         </figure>
 
         {{-- Aside du post --}}
-        @include('posts.aside', ['post' => $post->aside])
+        @include('categories._show', ['categorie' => $post->categorie])
 
         {{-- Comments du post --}}
         @include('comments._index', ['comments' => $post->comments])
