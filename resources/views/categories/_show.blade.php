88943fb9371cea594d116d8667d53da7a2fdaad8
halouma <essidahlem.info1997@gmail.com>
2022-03-09 20:52:09 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -13,7 +13,7 @@
       <section class="work">
         <div class="more">
           <figure class="white">
-              <a href="{{ route('categories._show', [
+              <a href="{{ route('categories.aside', [
                     'categorie' => $categorie->id,
                     'slug' => \Illuminate\Support\Str::slug($categorie->name)
                   ]) }}">
