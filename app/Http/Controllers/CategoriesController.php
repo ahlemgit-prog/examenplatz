de6e3ed3a68c5ff40106285bfc4316c2184db442
halouma <essidahlem.info1997@gmail.com>
2022-03-09 13:19:30 +0100

    Platz

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
+use App\Models\Categorie;
 
 class CategoriesController extends Controller
 {
-  public function show(Category $categorie) {
+  public function show(Categorie $categorie) {
         return view('categories._show', compact('categorie'));
     }
 }
