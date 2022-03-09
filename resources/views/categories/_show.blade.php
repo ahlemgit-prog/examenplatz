a3653a8d5c2143b95d5d52692914e6b6acf9fb11
halouma <essidahlem.info1997@gmail.com>
2022-03-09 20:20:14 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -10,8 +10,30 @@
   <div class="container object">
 
     <div id="main-container-image">
+      <section class="work">
+        @foreach($categories as $categorie)
+        <div class="more">
+          <figure class="white">
+              <a href="{{ route('categories._index', [
+                    'categorie' => $categorie->id,
+                    'slug' => \Illuminate\Support\Str::slug($categorie->name)
+                  ]) }}">
+                <img src="{{asset('storage/posts/' . $categorie->icon)}}" alt="" />
+                <dl>
+                  <dt>{{ $categorie->icon }}</dt>
+                  <dd>{{ $categorie->icon }}</dd>
+                </dl>
+              </a>
+                <div id="wrapper-part-info">
+                  <div class="part-info-image"><img src="{{asset('assets/img/' . $categorie->icon)}}" alt=""></div>
+                  <div id="part-info">{{ $categorie->name }}</div>
+                </div>
+          </figure>
+        </div>
+        @endforeach
+      </section>
 
-          {{ $categorie->posts }}
+
 
       </div>
     </div>
