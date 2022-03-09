11bbe2575411ddbdd898fa2ddf2f74719245acab
halouma <essidahlem.info1997@gmail.com>
2022-03-09 15:46:08 +0100

    Platz

--- resources/views/template/partials/_filter.blade.php
+++ resources/views/template/partials/_filter.blade.php
@@ -9,7 +9,29 @@
             <div class="small-logo"></div>
           </div>
 
-          @extends('categories._show')
-          
+          <div id="main-premium-ressource">
+              <div class="premium-ressource"><a href="{{ route('posts._premium') }}">Premium resources</a></div>
+          </div>
+
+          <div id="main-themes">
+              <div class="themes"><a href="{{ route('posts._theme') }}">Latest themes</a></div>
+          </div>
+
+          <div id="main-psd">
+              <div class="psd"><a href="{{ route('posts._psd') }}">PSD goodies</a></div>
+          </div>
+
+          <div id="main-ai">
+              <div class="ai"><a href="{{ route('posts._ai') }}">Illustrator freebies</a></div>
+          </div>
+
+          <div id="main-font">
+              <div class="font"><a href="{{ route('posts._font') }}">Free fonts</a></div>
+          </div>
+
+          <div id="main-photo">
+              <div class="photo"><a href="{{ route('posts._photo') }}">Free stock photos</a></div>
+          </div>
+
       </div>
   </div>
