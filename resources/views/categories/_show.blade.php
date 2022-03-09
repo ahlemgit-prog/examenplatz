b40f2efdd2aaa4ff3b07bf5848b5f3beea0c39ac
halouma <essidahlem.info1997@gmail.com>
2022-03-09 20:47:37 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -13,10 +13,7 @@
       <section class="work">
         <div class="more">
           <figure class="white">
-              <a href="{{ route('categories.main', [
-                    'categorie' => $categorie->id,
-                    'slug' => \Illuminate\Support\Str::slug($categorie->name)
-                  ]) }}">
+              <a href="{{ }}">
                 <img src="{{asset('storage/posts/' . $categorie->icon)}}" alt="" />
                 <dl>
                   <dt>{{ $categorie->icon }}</dt>
