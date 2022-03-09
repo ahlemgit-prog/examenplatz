c8a3682225e6d615d82360020f1bf9d6eed42e1e
halouma <essidahlem.info1997@gmail.com>
2022-03-09 16:32:08 +0100

    Platz

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,12 +10,4 @@ class CategoriesController extends Controller
   public function show(Category $categorie) {
         return view('categories._show', compact('categorie'));
     }
-
-    public function show()
-    {
-        $categories = Category::all();
-        $posts  = Post::all();
-
-        return view('categories._show', compact('posts', 'categories'));
-    }
 }
