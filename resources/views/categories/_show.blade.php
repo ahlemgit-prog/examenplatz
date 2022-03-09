ce890093fb428f04233033880c5cf7eca6af994a
halouma <essidahlem.info1997@gmail.com>
2022-03-09 22:40:33 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -9,26 +9,7 @@
 
   <div class="container object">
 
-    <div id="main-container-image">
-      <section class="work">
-        <div class="more">
-          <figure class="white">
-              <a href="{{ route('posts.show')}}">
-                <img src="{{asset('storage/posts/' . $categorie->posts)}}" alt="" />
-                <dl>
-                  <dt>{{ $categorie->name }}</dt>
-                  <dd>{{ $categorie->posts }}</dd>
-                </dl>
-              </a>
-                <div id="wrapper-part-info">
-                  <div class="part-info-image"><img src="{{asset('assets/img/' . $categorie->icon)}}" alt=""></div>
-                  <div id="part-info">{{ $categorie->name }}</div>
-                </div>
-          </figure>
-        </div>
-      </section>
-
-      </div>
+    {{ $categorie->posts}}
     </div>
 </div>
 @endsection
