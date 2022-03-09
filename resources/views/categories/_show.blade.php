992447cd4823f46d330a7db22654bac131475e0f
halouma <essidahlem.info1997@gmail.com>
2022-03-09 19:34:44 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -11,7 +11,28 @@
 
     <div id="main-container-image">
 
-          
+      <section class="work">
+        @foreach($categories as $categorie)
+        <div class="more">
+          <figure class="white">
+              <a href="{{ route('categories._show', [
+                    'categorie' => $categorie->id,
+                    'slug' => \Illuminate\Support\Str::slug($categorie->name)
+                  ]) }}">
+                <img src="{{asset('storage/posts/')}}" alt="" />
+                <dl>
+                  <dt></dt>
+                  <dd></dd>
+                </dl>
+              </a>
+                <div id="wrapper-part-info">
+                  <div class="part-info-image"><img src="{{asset('assets/img/')}}" alt=""></div>
+                  <div id="part-info">{{ $categorie->name }}</div>
+                </div>
+          </figure>
+        </div>
+        @endforeach
+      </section>
 
       </div>
     </div>
