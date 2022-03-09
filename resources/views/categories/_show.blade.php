0c0bae5e087eb892c652e42a6189fdafb94449c3
halouma <essidahlem.info1997@gmail.com>
2022-03-09 14:08:08 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -6,12 +6,12 @@
 
     <div class="wrapper-weight">
       <div class="icon-weight"><img src="{{asset('/assets/img/icon-weight.svg')}}" alt="" width="20" height="23"/></div>
-        <div class="text-weight">{{ $categorie->posts->size }} Mo</div>
+        <div class="text-weight">{{ $post->size }} Mo</div>
     </div>
 
     <div class="wrapper-desc">
       <div class="icon-desc"><img src="{{asset('/assets/img/icon-desc.svg')}}" alt="" width="24" height="24"/></div>
-        <div class="text-desc">{{ $categorie->posts->excerpt }} </div>
+        <div class="text-desc">{{ $post->excerpt }} </div>
     </div>
 
     <div class="wrapper-download">
