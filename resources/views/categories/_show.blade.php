d0f1e6a307098c34d06e37975f89dc9a71e582c2
halouma <essidahlem.info1997@gmail.com>
2022-03-10 10:35:28 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -15,7 +15,7 @@
               <a href="{{ route('categories.content', [
                     'categorie' => $categorie->id,
                     'slug' => \Illuminate\Support\Str::slug($categorie->name),
-                    'posts' => $categorie->slug
+                    'posts' => $categorie->category_id
                   ]) }}">
                 <img src="{{asset('storage/posts/' . $categorie->posts)}}" alt="" />
                 <dl>
