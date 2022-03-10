5fd1ae5dbcd1c438f700856a5606d9183e72ecf9
halouma <essidahlem.info1997@gmail.com>
2022-03-10 18:01:46 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -25,7 +25,7 @@
                 </dl>
               </a>
                 <div id="wrapper-part-info">
-                  <div class="part-info-image"><img src="{{asset('assets/img/' . $post->categories->icon)}}" alt=""></div>
+                  <div class="part-info-image"><img src="{{asset('assets/img/' . $post->meta_description)}}" alt=""></div>
                   <div id="part-info">{{ $post->categories->name }}</div>
                 </div>
             </figure>
