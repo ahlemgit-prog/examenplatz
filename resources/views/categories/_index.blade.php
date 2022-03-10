9d68187e83247c1d87262cf7308c5ae7db81607b
halouma <essidahlem.info1997@gmail.com>
2022-03-10 13:35:10 +0100

    Platz

--- resources/views/categories/_index.blade.php
+++ resources/views/categories/_index.blade.php
@@ -4,28 +4,10 @@
   <div id="bouton-ai">
     <ul>
       <li>
-        <a href="{{ route('categories._show', ['categorie' => $categorie->id, 'slug' => Illuminate\Support\Str::slug($categorie->name)]) }} " id="more-posts" data-url="{{ route('ajax.posts.more') }}">
+        <a href="{{ route('categories._show', ['categorie' => $categorie->id, 'slug' => Illuminate\Support\Str::slug($categorie->name)]) }} ">
           <img src=" {{asset('storage/posts/' . $categorie->icon)}}" alt="{{$categorie->name}}" title="{{$categorie->name}}" height="28" width="28">
         </a>
       </li>
     </ul>
   </div>
 @endforeach
-
-@section('scripts')
-
-    <script type="text/javascript">
-
-    let offset = 0;
-    $('#more-posts').click(function(e) {
-      e.preventDefault();
-      $.get($(this).data('url'), {offset: offset})
-       .done(function(rep) {
-         $('#list').append(rep)
-                   .find('div:nth-last-of-type(-n+10)');
-          offset = offset + 0;
-       });
-    });
-    </script>
-
-@endsection
