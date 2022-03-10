3b19c2a82e44005131a2977c96282e5f57206b7a
halouma <essidahlem.info1997@gmail.com>
2022-03-10 16:12:12 +0100

    Platz

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,11 +7,14 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-  public function show(Category $categorie) {
-        return view('categories._show', compact('categorie'));
-    }
-
   public function content(Category $categorie) {
         return view('categories.content', compact('categorie'));
     }
+
+    public function show() {
+      $categories = Category::orderBy('id', 'asc')
+                   ->take(20)
+                   ->get();
+      return view('categories._show', compact('categories'));
+  }
 }
