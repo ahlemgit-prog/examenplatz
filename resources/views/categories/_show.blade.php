dc954345a7d57e8f312e5b3a1a39a94f49613667
halouma <essidahlem.info1997@gmail.com>
2022-03-10 15:44:57 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -15,37 +15,4 @@
     </div>
   </div>
 </div>
-
-<div id="wrapper-oldnew">
-  <div class="oldnew">
-    <div  class="wrapper-oldnew-more">
-      <ul>
-        <li>
-          <a href="#" id="more-categories" data-url="{{ route('ajax.categories.more') }}">
-            <img id="oldnew-more" src="{{ asset('/assets/img/more.png') }}" alt="">
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-@endsection
-
-
-@section('scripts')
-
-    <script type="text/javascript">
-
-    let offset = 0;
-    $('#more-categories').click(function(e) {
-      e.preventDefault();
-      $.get($(this).data('url'), {offset: offset})
-       .done(function(rep) {
-         $('#list').append(rep)
-                   .find('div:nth-last-of-type(-n+10)');
-          offset = offset + 0;
-       });
-    });
-    </script>
-
 @endsection
