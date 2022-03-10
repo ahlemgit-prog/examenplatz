ba4156b95cfac3fcde56725680676c8dcf6463d4
halouma <essidahlem.info1997@gmail.com>
2022-03-10 12:50:08 +0100

    Platz

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/ajax/comments/add', [\App\Http\Controllers\CommentsController::clas
 
 // Route des catégories
 
-Route::get('/posts/{post}/{slug}', [\App\Http\Controllers\CategoriesController::class, 'show'])
+Route::get('/categories/{categorie}/{slug}', [\App\Http\Controllers\CategoriesController::class, 'show'])
      ->name('categories._show');
 
 Route::get('/categories/{categorie}/{slug}/posts/{posts}', [\App\Http\Controllers\CategoriesController::class, 'content'])
