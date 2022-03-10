8b05d6907d7448e95395ef0832bad7ed88882fcf
halouma <essidahlem.info1997@gmail.com>
2022-03-10 12:24:33 +0100

    Platz

--- resources/views/categories/_show.blade.php
+++ resources/views/categories/_show.blade.php
@@ -5,31 +5,3 @@
 @endsection
 
 @yield('content')
-
-<!-- <div id="wrapper-container">
-  <div class="container object">
-    <div id="main-container-image">
-      <section class="work">
-        <div class="more">
-          <figure class="white">
-              <a href="{{ route('posts.show', [
-                    'post' => $post->id,
-                    'slug' => \Illuminate\Support\Str::slug($post->slug),
-                    'posts' => $post->slug
-                  ]) }}">
-                <img src="{{asset('storage/posts/' . $categorie->image)}}" alt="" />
-                <dl>
-                  <dt>{{ $categorie->name }}</dt>
-                  <dd>{{ $categorie->name }}</dd>
-                </dl>
-              </a>
-                <div id="wrapper-part-info">
-                  <div class="part-info-image"><img src="{{asset('assets/img/' . $categorie->icon)}}" alt=""></div>
-                  <div id="part-info">{{ $categorie->name }}</div>
-                </div>
-          </figure>
-        </div>
-      </section>
-    </div>
-  </div>
-</div> -->
