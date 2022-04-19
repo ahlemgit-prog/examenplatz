c24da7d81265cbc16aca306ef527d75fed0b278b
halouma <essidahlem.info1997@gmail.com>
2022-04-19 12:14:12 +0200

    Platz

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,7 @@ class PostsController extends Controller
 {
   public function index() {
     $posts = Post::orderBy('created_at', 'desc')
-                 ->take(20)
+                 ->take(12)
                  ->get();
     return view('posts.index', compact('posts'));
 }
